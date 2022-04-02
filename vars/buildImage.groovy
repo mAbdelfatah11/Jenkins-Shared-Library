0c47053e105a58e410f7717c900fca573548dce3
mAbdelfatah11 <mahmoud.abdelfatah707@gmail.com>
2022-04-02 18:09:28 +0200

    update the buildimage job

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,9 +3,9 @@
 def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'Dockerhub-Credentails', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t mabdelfatah/mma-demo:java-maven-app-v1 .'
+        sh "docker build -t mabdelfatah/mma-demo:${IMAGE_NAME} ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push mabdelfatah/mma-demo:java-maven-app-v1'
+        sh "docker push mabdelfatah/mma-demo:${IMAGE_NAME}"
     }
 
 }
