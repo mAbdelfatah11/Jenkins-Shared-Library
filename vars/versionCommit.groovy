d1792d784030bfc9ab39b5b0ef06bad185b91e36
Mahmoud Abdelfatah <89046475+mAbdelfatah11@users.noreply.github.com>
2022-04-04 05:56:27 +0200

    Update versionCommit.groovy

--- vars/versionCommit.groovy
+++ vars/versionCommit.groovy
@@ -5,10 +5,10 @@
             withCredentials([usernamePassword(credentialsId: 'Github-Credentails', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
                 // git config here for the first time run
                 // AS ALternative: you can configure the author configs. in the jenkins server itself.
-                //sh 'git config --global user.email "jenkins@widebot.net"'
-                //sh 'git config --global user.name "jenkins"'
+                sh 'git config --global user.email "jenkins@widebot.net"'
+                sh 'git config --global user.name "jenkins"'
                 
-                //sh "git remote add origin https://${USER}:${PASS}@github.com/mAbdelfatah11/java-maven-app-jenkins.git"
+                sh "git remote set-url origin https://${USER}:${PASS}@github.com/mAbdelfatah11/java-maven-app-jenkins.git"
                 sh 'git add .'
                 sh 'git commit -m "ci: version bump"'
                 sh 'git push origin HEAD:Jenkins-Using-SharedLibraray-to-ManageJobs'
